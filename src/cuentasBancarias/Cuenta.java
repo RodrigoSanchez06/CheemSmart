f8e5b9275941a1b8f6ba941c3bc35accb7029a83
RodrigoSanchez06 <rogerxdgta@ciencias.unam.mx>
2022-10-25 09:42:40 -0500

    add cuenta

---
+++ src/cuentasBancarias/Cuenta.java
@@ -0,0 +1,43 @@
+package cuentasBancarias;
+
+import cliente.Cliente;
+
+/**
+ * Clase que representa a una cuenta bancaria de un cliente asociado a CheemSmart
+ */
+public class Cuenta implements Icuenta{
+
+    private double fondos;
+    private int noCuenta;
+    private int nip;
+    private Cliente clienteAsociado;
+
+    public Cuenta(double fondos,int noCuenta, int  nip, Cliente clienteAsociado){
+        this.fondos = fondos;
+        this.noCuenta = noCuenta;
+        this.nip = nip;
+        this.clienteAsociado = clienteAsociado;
+    }
+
+    @Override
+    public void comprar(int factura, int noCuenta, int nip) {
+        if (logIn(noCuenta, nip)) {
+            fondos = fondos - factura;
+            consutarFondos();   
+        } else {
+            System.out.println("El numero de cuenta o el nip, son incorrectos.");
+        }
+    }
+
+    @Override
+    public void consutarFondos(){
+        System.out.println("Saldo de la cuenta: " + fondos);
+    }
+
+    private boolean logIn(int noCuenta, int nip){
+        if (noCuenta == this.noCuenta && nip == this.nip) {
+            return true;
+        }
+        return false;
+    }
+}
