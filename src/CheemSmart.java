9d1684c00cd8289bca7d84e55ccd0258ef781dab
RodrigoSanchez06 <rogerxdgta@ciencias.unam.mx>
2022-10-25 20:46:56 -0500

    add: main

--- src/CheemSmart.java
+++ src/CheemSmart.java
@@ -1,5 +1,56 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+import cliente.Cliente;
+import cuentasBancarias.Cuenta;
+import idiomas.EspanniolEsp;
+import idiomas.EspanniolLatm;
+import idiomas.Ingles;
+
 public class CheemSmart{
     public static void main(String[] args) {
-        
+
+        LinkedList<Cliente> listaClientes = agregaClientes();
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("\n******Bienvenido a CheemSmart******\n");
+        System.out.println("\n Para entrar a la tienda virtual Inicia Sesión.\n");
+        System.out.println("Introduce tu Nombre de usuario: ");
+        String nombreUsuario = entrada.nextLine();
+        System.out.println("Introduce la contraseña: ");
+        String password = entrada.nextLine();
+        for (Cliente c : listaClientes) {
+            
+        }
+    }
+
+    public static LinkedList<Cliente> agregaClientes(){
+        LinkedList<Cliente> listaClientes = new LinkedList<Cliente>();  
+        EspanniolLatm latm = new EspanniolLatm();
+        EspanniolEsp esp = new EspanniolEsp();
+        Ingles ing = new Ingles();
+        int id = 0;
+
+        Cliente clienteUno = new Cliente("JuanitoPro777", "juan1234", "Juan Perez",
+            552509051, "Nativitas CDMX", null,latm , id);
+        clienteUno.setCuentaAsociada(crearAsociarCuenta(20000, 1234, clienteUno, 22060325));
+
+        Cliente clienteDos = new Cliente("Maria1721", "mar1a_12", "María Fernanda",
+             23123625, "Barcelona España", null, esp, ++id);
+        clienteDos.setCuentaAsociada(crearAsociarCuenta(12500, 1721, clienteDos, 17118123));
+
+        Cliente clienteTres = new Cliente("Fred12", "fr3di_21", "Frederick Town",
+            7853255, "Texas 5th AV.", null, ing, ++id);
+        clienteDos.setCuentaAsociada(crearAsociarCuenta(125, 0001, clienteTres, 31922257));
+
+        listaClientes.add(clienteUno);
+        listaClientes.add(clienteDos);
+        listaClientes.add(clienteTres);
+
+        return listaClientes;
+    }
+
+    public static Cuenta crearAsociarCuenta(double fondos, int nip, Cliente clienteAsociado, int numCuenta){
+        return new Cuenta(fondos, numCuenta, nip, clienteAsociado);    
     }
 }
---
+++ src/idiomas/Ingles.java
@@ -0,0 +1,67 @@
+package idiomas;
+
+/**
+ * Clase que Representa al idioma Inglés del sistema CheemSmart.
+ */
+public class Ingles implements Idioma{
+
+    /**
+     * Saluda al usuario en el idioma actual.
+     */
+    @Override
+    public void saludo() {
+        System.out.println("Welcome to CheemSmart!!!.");
+    }
+
+    /**
+     * Despide al usuario en el idioma actual.
+     */
+    @Override
+    public void despedida() {
+        System.out.println("See you later!!! :)");
+    }
+
+    /**
+     * Da a conocer los descuentos disponibles en la región
+     * en el idioma actual.
+     */
+    @Override
+    public void descuentoPropaganda() {
+        System.out.println("today's deals");
+    }
+
+    /**
+     * Da a conocer las opciones disponibles en el idioma actual.
+     */
+    @Override
+    public void opciones() {
+        System.out.println("Choose one of the following options: "
+        + "\n 1. Read catalogue"
+        + "\n 2. Purchase"
+        + "\n 3. Exit");
+    }
+
+    /**
+     * Hace saber al cliente que su compra fue completada con éxito.
+     */
+    @Override
+    public void compraCompletada() {
+        System.out.println("Thank you so much, your ticket will be printed soon");
+    }
+
+    /**
+     * Hace saber al usuario que sus datos bancarios son incorrectos.
+     */
+    @Override
+    public void datosIncorrectos() {
+        System.out.println("Sorry but your account number or pin doesn't match");
+    }
+
+    /**
+     * Hace saber al usuario que la opción que introdujo fue incorrecta.
+     */
+    @Override
+    public void opcionIncorrecta() {
+        System.out.println("Incorrect option, try again!");
+    }
+}
