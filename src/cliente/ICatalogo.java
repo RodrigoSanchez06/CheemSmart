549e6511ae986ab105c42065af1fdd1418a62141
Chao280802 <romerocruzfernando@gmail.com>
2022-10-28 18:54:47 -0500

    Catalago y proxy implementados

--- src/cliente/ICatalogo.java
+++ src/cliente/ICatalogo.java
@@ -1,24 +1,10 @@
 package cliente;
 
-import java.util.LinkedList;
 import java.util.Iterator;
 import catalogo.Departamento;
 
 public interface ICatalogo {
-    /**
-     * Crea una linkedList con los diferentes departamentos que existen.
-     * 
-     * @param alimentos         menu de lujo.
-     * @param electrodomesticos menu versatil.
-     * @param Electronicos      menu tradicional.
-     * @return lista con todos los departamentos que existen.
-     */
-    public static Iterator<Departamento> subDepartamentos(Departamento alimentos, Departamento electrodomesticos,
-            Departamento electronicos) {
-        LinkedList<Departamento> sDepartamentos = new LinkedList<Departamento>();
-        sDepartamentos.add(alimentos);
-        sDepartamentos.add(electrodomesticos);
-        sDepartamentos.add(electronicos);
-        return sDepartamentos.iterator();
-    }
+    Iterator<Departamento> iteradorDepartamentos();
+
+    void imprimirCatalogo();
 }
