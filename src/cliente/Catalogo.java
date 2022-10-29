2e8d222cec3dd87e157b14429a94465829dfa037
Chao280802 <romerocruzfernando@gmail.com>
2022-10-28 19:03:44 -0500

    Sin comentario

--- src/cliente/Catalogo.java
+++ src/cliente/Catalogo.java
@@ -8,10 +8,6 @@ import catalogo.alimentos.Alimentos;
 import catalogo.electronicos.Electronicos;
 
 public class Catalogo implements ICatalogo{
-    // hacer una clase para alimentos
-    // hacer una clase para electrodomesticos, electronicos y alimentos, en su
-    // constructor le agrego los productos de su categoria. y les hago un metodo
-    // para que devuelvan su iterador. el menu lo cambio a departamento
 
     Departamento departamentoAlimentos = new Alimentos();
     Departamento departamentoElectronicos = new Electronicos();
